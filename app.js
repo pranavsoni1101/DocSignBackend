b6ac2ee2d5dfce2c69bf54edd9d7ed49deff0146
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-05 15:27:04 +0530

    Added cors to mitigate cors error

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const connectToDb = require('./db');
 const authRouter = require("./routes/auth");
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', authRouter);
 // Connect to MongoDB
